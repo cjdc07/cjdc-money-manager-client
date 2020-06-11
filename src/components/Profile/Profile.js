8fd8351b9c8b7f9e26a578fb8ec659b9ecccc1bc
Christian Joshua Catalan <cj.catalan07@gmail.com>
2020-06-11 21:41:20 +0800

    Add Gmail login and some bug fixes (#5)

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import { AUTH_TOKEN } from '../../constants';
+import { GoogleLogout } from 'react-google-login';
 import { withRouter } from 'react-router';
 import { withApollo } from 'react-apollo';
 
 function Profile({ history, onLogin, client }) {
+  const logout = (response) => {
+    localStorage.removeItem(AUTH_TOKEN);
+    client.resetStore();
+    onLogin(false);
+    history.push({ pathname: '/login' });
+  }
+
   return (
     <div className="flex flex-column h-100 justify-center items-center ph3 center w-50-ns">
-      <button
-        className="w-100 bw0 pa2 mb3 white bg-green"
-        onClick={() => {
-          localStorage.removeItem(AUTH_TOKEN);
-          client.resetStore();
-          onLogin(false);
-          history.push({ pathname: '/login' });
-        }}
+      <GoogleLogout
+        clientId={process.env.REACT_APP_GMAIL_CLIENT_ID}
+        buttonText="Logout"
+        onLogoutSuccess={logout}
+        onFailure={logout}
+        render={(renderProps) => (
+          <button
+            onClick={renderProps.onClick}
+            disabled={renderProps.disabled}
+            className="w-100 bw0 pa2 mb3 white bg-green"
+          >
+            Logout
+          </button>
+        )}
       >
-        Logout
-      </button>
+      </GoogleLogout>
     </div>
   )
 }
