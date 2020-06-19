74d3bbcb249f55ef74f4114f9e1480364c620b87
Christian Joshua Catalan <cj.catalan07@gmail.com>
2020-06-20 00:51:45 +0800

    Improve modal position

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,8 +4,8 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 function Modal({ children, close, title }) {
   return (
-    <div className="fixed w-100 h-100 top-0 pv5 ph3 bg-black-40 z-999">
-      <div className="flex flex-column w-100 br2 bg-white shadow-2 pb3 max-h-80">
+    <div className="fixed w-100 h-100 top-0 bg-black-40 z-999">
+      <div className="absolute bottom-0 flex flex-column w-100 br3 br--top bg-white shadow-2 max-h-80">
         <div className="pa3 bb b--black-30">
           <span className="f4 b fl">{title}</span>
           <span className="f4 fr" onClick={() => close()}><FontAwesomeIcon icon={faTimes} /></span>
