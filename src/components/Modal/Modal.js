f0e301afd3f8e960ca05f25af2139d8d882426f0
Christian Joshua Catalan <cj.catalan07@gmail.com>
2020-06-20 01:11:03 +0800

    Add padding on modal window

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,7 +5,7 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'
 function Modal({ children, close, title }) {
   return (
     <div className="fixed w-100 h-100 top-0 bg-black-40 z-999">
-      <div className="absolute bottom-0 flex flex-column w-100 br3 br--top bg-white shadow-2 max-h-80">
+      <div className="absolute bottom-0 flex flex-column w-100 br3 br--top bg-white shadow-2 max-h-80 pb3">
         <div className="pa3 bb b--black-30">
           <span className="f4 b fl">{title}</span>
           <span className="f4 fr" onClick={() => close()}><FontAwesomeIcon icon={faTimes} /></span>
